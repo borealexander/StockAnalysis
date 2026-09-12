4a222579650ac7567e68ac29e6d1b6b3b5adf12c
borealexander <alexander.bore@live.se>
2026-09-12 16:24:33 +0200

    added portfolio manager

---
+++ src/manager/PortfolioManager.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+class PortfolioManager:
+    def __init__(self, reader):
+
+        self.reader = reader
+
+    def create_portfolio(self):
+        portfolio = 1
+        return self
+
+    def update_portfolio(self):
+
+        return self
